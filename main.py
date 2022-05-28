ad79999f9ad80c3575597447311e2eb7a71ed27c
cinderbelt <2krok@snu.ac.kr>
2022-05-29 00:23:17 +0900

    second commit almost done but there are several error to fix

--- main.py
+++ main.py
@@ -1,78 +1,140 @@
 import random
-class player():
+
+class playerclass():
     def __init__(self,name):
         self.score=0
         self.name=name
 
     def pitching(self):
+        global pitch
         if self == com:
-            pitch= random.randint(0,1)
+            self.pitch= random.randint(0,1)
         else:
-            pitch = int(input('pitch'))
+            self.pitch = int(input('pitch'))
 
 
     def batting(self):
+        global bat
         if self == com:
             if outcounts != 2:
-                bat= random.randint(0,2)
+                self.bat= random.randint(0,2)
 
             else:
-                bat= random.randint(0,1)
+                self.bat= random.randint(0,1)
 
         else:
-            bat= int(input('bat'))
+            self.bat= int(input('bat'))
 
 
 
 def game(atk,defend):
     global outcounts
+    global inning
+    inning =1
     outcounts =0
     strike=0
     ball=0
-    base=[0,0,0,0]
+    base=[]
     while outcounts <3:
         atk.batting()
         defend.pitching()
         if atk.bat != 2:
             if defend.pitch != atk.bat:
                 strike+=1
+                print('strike'+str(strike))
             elif defend.pitch==0:
                 ball+=1
+                print(str(ball)+'ball')
 
             else:
                 if base != [1,1,1,1]:
                     base.append(1)
+                    print('hit')
 
                 else:
                     atk.score+=1
+                    print(atk.name+'score'+atk.score)
         else:
             if defend.pitch == 1:
-                atk.score += sum(base)
-                base=[0,0,0,0]
+                if base != []:
+                    atk.score += sum(base)
+                else:
+                    atk.score += 1
+                print('homerun')
+                print(atk.name+'score'+str(atk.score))
+                base=[]
+                strike=0
+                ball=0
             else:
                 outcounts =3
 
         if strike ==2:
             outcounts +=1
             strike=0
+            ball =0
+            print('strike out')
+            print(str(outcounts)+'out')
         if ball ==3:
-            if base != [1,1,1,1]
+            if base != [1,1,1,1]:
                 base.append(1)
                 ball=0
+                strike =0
+                print('base on ball')
+
             else:
                 atk.score+=1
                 ball=0
+                strike =0
+                print('base on ball')
+                print(atk.name + 'score' + str(atk.score))
 
+        if outcounts == 3:
+            inning+=0.5
+            print('three out change')
 def main():
     playername=input('input your name')
     tb=input('top or bottom')
-    player=player(playername)
-    com=player('com')
-    inning=1
+    player= playerclass(playername)
+    inning =1
+    global com
+    com=playerclass('com')
     if tb== 'top':
-       while outcounts <3:
-           game(player,com)
-           if outcounts==3:
+       while inning <4:
+           game(player, com)
+           game(com,player)
+           if inning==3.5 and (player.score < com.score):
+              print('com win')
+              break
+           if (player.score > com.score) and inning ==4:
+               print('player win')
+               break
+           elif (player.score == com.score) and inning ==4:
+               print('draw')
+               break
+
+    if tb== 'bottom':
+        while inning < 4:
+            game(com, player)
+            game(player, com)
+            if inning == 3.5 and (player.score > com.score):
+                print('player win')
+                break
+
+            if (player.score < com.score) and inning ==4:
+                print('com win')
+                break
+
+            elif (player.score == com.score) and inning ==4:
+                print('draw')
+                break
+
+main()
+
+
+
+
+
+
 
 
 
